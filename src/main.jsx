2523a3b0f1441b039c644d8992b9140524ff809e
andybarba22-OHC <andybarba22@gmail.com>
2026-05-15 15:15:25 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import OHC from './OHC.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <OHC />
+  </React.StrictMode>
+)
